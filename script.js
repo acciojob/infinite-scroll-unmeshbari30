e73cec6994ccbbb3b6f251f77ee869bd5b5765f3
Unmesh Bari <unmeshbari5@gmail.com>
2026-05-13 23:42:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your code here!
 
+const list = document.getElementById("infi-list");
+
+let itemCount = 0;
+
+function addItems(count) {
+  for (let i = 0; i < count; i++) {
+    itemCount++;
+
+    const li = document.createElement("li");
+    li.textContent = `Item ${itemCount}`;
+
+    list.appendChild(li);
+  }
+}
+
+addItems(10);
+
+
+list.addEventListener("scroll", () => {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    addItems(2);
+  }
+});
